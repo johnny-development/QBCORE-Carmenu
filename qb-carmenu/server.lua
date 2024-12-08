5f2dc9f623550c6c4e2caf37a6c2798f1b99a81f
johnny-development <bananarenan5@gmail.com>
2024-12-08 21:40:40 +1100

    ignore

--- qb-carmenu/server.lua
+++ qb-carmenu/server.lua
@@ -1,10 +1,1 @@
-QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Functions.CreateCallback('qb-vehiclemenu:canSpawn', function(source, cb)
-    local Player = QBCore.Functions.GetPlayer(source)
-    if Player and Player.PlayerData.job.name == "mechanic" then
-        cb(true)
-    else
-        cb(false)
-    end
-end)
